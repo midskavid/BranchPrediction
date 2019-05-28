6d4f122646076f33ff29a66c73b45f90b72c8785
midskavid <midskavid@yahoo.co.in>
2019-05-27 19:25:35 -0700

    Tournament changes

--- src/predictor.c
+++ src/predictor.c
@@ -29,8 +29,13 @@ int lhistoryBits; // Number of bits used for Local History
 int pcIndexBits;  // Number of bits used for PC index
 int bpType;       // Branch Prediction Type
 int verbose;
-unsigned int *historyTable;
+
+unsigned int *globalPredictionTable;
+unsigned int *localPredictionTable;
+unsigned int *localHistoryTable;
+unsigned int *selector;
 unsigned int globalHistory;
+
 //------------------------------------//
 //      Predictor Data Structures     //
 //------------------------------------//
@@ -57,15 +62,38 @@ init_predictor()
       return;
     case GSHARE : 
     {
-      historyTable = (unsigned int*)malloc((int)pow(2.0,(double)ghistoryBits)*sizeof(unsigned int));
+      globalPredictionTable = (unsigned int*)malloc((int)pow(2.0,(double)ghistoryBits)*sizeof(unsigned int));
+      
+      // Initialize all to one..
       int len = (int)pow(2.0,(double)ghistoryBits);
       for (int ii = 0; ii < len; ++ii)
-        historyTable[ii] = 0;
+        globalPredictionTable[ii] = 1;
       globalHistory = 0;
       return;
     }
     case TOURNAMENT :
+    {
+      globalPredictionTable = (unsigned int*)malloc((int)pow(2.0,(double)ghistoryBits)*sizeof(unsigned int));
+      localPredictionTable = (unsigned int*)malloc((int)pow(2.0,(double)lhistoryBits)*sizeof(unsigned int));
+      localHistoryTable = (unsigned int*)malloc((int)pow(2.0,(double)pcIndexBits)*sizeof(unsigned int));
+      selector = (unsigned int*)malloc((int)pow(2.0,(double)ghistoryBits)*sizeof(unsigned int));
+      // initialize all to 1..
+      int len = (int)pow(2.0,(double)ghistoryBits);
+      for (int ii = 0; ii < len; ++ii){
+        globalPredictionTable[ii] = 1;
+        selector[ii] = 2; // Check what happens with other values!!!
+      }
+
+      len = (int)pow(2.0,(double)lhistoryBits);
+      for (int ii = 0; ii < len; ++ii)
+        localPredictionTable[ii] = 1;
+
+      len = (int)pow(2.0,(double)pcIndexBits);
+      for (int ii = 0; ii < len; ++ii)
+        localHistoryTable[ii] = 1;
+      globalHistory = 0;
       return;
+    }
   }
 }
 
@@ -89,10 +117,28 @@ make_prediction(uint32_t pc)
       // XOR and then index onto the table....
       // Is is MSB or LSB
       int indexTable = (int)((pc & ((1 << ghistoryBits) - 1)) ^ (globalHistory & ((1 << ghistoryBits) - 1)));
-      return ((historyTable[indexTable] & (1 << 1)) >> 1) ? TAKEN : NOTTAKEN;
+      return ((globalPredictionTable[indexTable] & (1 << 1)) >> 1) ? TAKEN : NOTTAKEN;
       break;
     }
     case TOURNAMENT:
+    {
+      // printf("%djojojoj\n", (pc & ((1 << pcIndexBits) - 1)));
+      // printf("%d\n", localHistoryTable[(pc & ((1 << pcIndexBits) - 1))]);
+      // printf("%d\n", ((1 << lhistoryBits) - 1));
+      // printf("***********\n");
+      int indexTable = localHistoryTable[pc & ((1 << pcIndexBits) - 1)] & ((1 << lhistoryBits) - 1);
+      int localPrediction = localPredictionTable[indexTable];
+      int globalPrediction = globalPredictionTable[globalHistory & ((1 << ghistoryBits) - 1)];
+      if ((selector[globalHistory & ((1 << ghistoryBits) - 1)] & (1 << 1)) >> 1){
+        // Global Prediction..
+        return ((globalPrediction & (1 << 1)) >> 1) ? TAKEN : NOTTAKEN;
+      }
+      else{
+        // Local Prediction..
+        return ((localPrediction & (1 << 1)) >> 1) ? TAKEN : NOTTAKEN;
+      }
+      break;
+    }
     case CUSTOM:
     default:
       break;
@@ -119,20 +165,59 @@ train_predictor(uint32_t pc, uint8_t outcome)
     {
       int indexTable = (int)((pc & ((1 << ghistoryBits) - 1)) ^ (globalHistory & ((1 << ghistoryBits) - 1)));
       globalHistory = (globalHistory << 1) | outcome;
-      if (historyTable[indexTable] == SN) {
+      if (globalPredictionTable[indexTable] == SN) {
         if (outcome)
-          historyTable[indexTable] += 1;
+          globalPredictionTable[indexTable] += 1;
       }
-      else if (historyTable[indexTable] == ST) {
+      else if (globalPredictionTable[indexTable] == ST) {
         if (!outcome) {
-          historyTable[indexTable] -= 1;
+          globalPredictionTable[indexTable] -= 1;
         }
       }
       else
-        historyTable[indexTable] += outcome ? 1 : -1;
+        globalPredictionTable[indexTable] += outcome ? 1 : -1;
       break;
     }
     case TOURNAMENT:
+    {
+      int indexTable = localHistoryTable[(int)(pc & ((1 << pcIndexBits) - 1))] & ((1 << lhistoryBits) - 1);
+
+      int localPrediction = localPredictionTable[indexTable];
+      int globalPrediction = globalPredictionTable[globalHistory & ((1 << ghistoryBits) - 1)];
+
+      if (outcome){
+        // Global Prediction..
+        if ((localPrediction >= 2) && (globalPrediction <=1) || (localPrediction <=1 && globalPrediction >=2)){
+          // check who predicted..
+          if ((globalPrediction > 1) && (selector[globalHistory & ((1 << ghistoryBits) - 1)]) != 3)
+            selector[globalHistory & ((1 << ghistoryBits) - 1)]++;
+          else if ((localPrediction > 1 ) && (selector[globalHistory & ((1 << ghistoryBits) - 1)] !=0))
+            selector[globalHistory & ((1 << ghistoryBits) - 1)]--;
+        }
+        if (localPrediction != 3)
+          ++localPredictionTable[indexTable];
+        if (globalPrediction != 3)
+          ++globalPredictionTable[globalHistory & ((1 << ghistoryBits) - 1)];
+      }
+      else{
+        // Local Prediction..
+        if ((localPrediction >= 2) && (globalPrediction <=1) || (localPrediction <=1 && globalPrediction >=2)){
+          // check who predicted..
+          if ((globalPrediction < 1) && (selector[globalHistory & ((1 << ghistoryBits) - 1)]) != 3)
+            selector[globalHistory & ((1 << ghistoryBits) - 1)]++;
+          else if ((localPrediction < 1 ) && (selector[globalHistory & ((1 << ghistoryBits) - 1)] !=0))
+            selector[globalHistory & ((1 << ghistoryBits) - 1)]--;
+        }
+        if (localPrediction != 0)
+          --localPredictionTable[indexTable];
+        if (globalPrediction != 0)
+          --globalPredictionTable[globalHistory & ((1 << ghistoryBits) - 1)];
+      }
+      globalHistory = (globalHistory << 1) | outcome; 
+      // Update localHistoryTable
+      localHistoryTable[pc & ((1 << pcIndexBits) - 1)] = (localHistoryTable[pc & ((1 << pcIndexBits) - 1)] << 1) | outcome; 
+      break;    
+    }
     case CUSTOM:
     default:
       break;
@@ -144,9 +229,13 @@ void delete_predictor(){
     case STATIC :
       return;
     case GSHARE : 
-      free(historyTable);
+      free(globalPredictionTable);
       return;
     case TOURNAMENT :
+      free(globalPredictionTable);
+      free(localPredictionTable);
+      free(localHistoryTable);
+      free(selector);
       return;
   }
 }
